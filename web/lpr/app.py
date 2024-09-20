c00deee378342e3450fd9e6de9c80accc8a49f2d
nils-holmberg <nils.holmberg@outlook.com>
2024-09-20 08:40:16 +0200

    open science methods @ 240920-084016

--- web/lpr/app.py
+++ web/lpr/app.py
@@ -14,7 +14,36 @@ def index():
 
 @app.route('/page1')
 def page1():
-    return '<h1>This is Page 1</h1>'
+    return """<h1>This is Page 1</h1>
+<!-- Start Landing Page -->
+      <div class="landing-page">
+        <header>
+          <div class="container">
+            <a href="#" class="logo">Your <b>Website</b></a>
+            <ul class="links">
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Work</li>
+              <li>Info</li>
+              <li>Get Started</li>
+            </ul>
+          </div>
+        </header>
+        <div class="content">
+          <div class="container">
+            <div class="info">
+              <h1>Looking For Inspiration</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
+              <button>Call to action</button>
+            </div>
+            <div class="image">
+              <img src="./001234.png">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Landing Page -->
+"""
 
 @app.route('/page2')
 def page2():
